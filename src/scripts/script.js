8466db23c3176e7f9a5e89c76a7b39cdef7384f0
unknown <huuthuat1911@gmail.com>
2019-08-15 16:16:07 +0700

    Update code html, css date 15/08

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,112 +1,129 @@
 
-    $(document).ready(function(){
-        var owl = $('#show_local');
-	owl.owlCarousel({
-		loop:false,
-		nav:false,
-		dots:false,
-		margin:10,
-		mouseDrag:true,
-		touchDrag:true,
-		responsive:{
-			720:{
-				items:3
-			},
-			320:{
-				items:1
-			}
-		}
-    });
-    });
+function openRightMenu() {
+    // document.getElementById("rightMenu").style.display = "block";
+}
 
-    function openRightMenu() {
-        // document.getElementById("rightMenu").style.display = "block";
+function closeRightMenu() {
+    // document.getElementById("rightMenu").style.display = "none";
+}
+$(document).ready(function () {
+    slide_Show();
+    show_Fillter();
+    CONTROL = {
+        main_LEFT: $('#container_main').offset().left,
+        main_sidenav: $('.main_sidenav '),
     }
-    
-    function closeRightMenu() {
-        // document.getElementById("rightMenu").style.display = "none";
+
+    CONTROL.main_sidenav.css({
+        'right': CONTROL.main_LEFT,
+    });
+    $('.btn_sidenav').click(function () {
+        CONTROL.main_sidenav.css({
+            'width': '270px',
+        });
+        $('.ovelay_menu').css({
+            'right': '270px',
+            'opacity': '1',
+            'visibility': 'visible'
+        });
+    });
+    $('#close_menu').click(function () {
+        CONTROL.main_sidenav.css({
+            'width': '0',
+        });
+        $('.ovelay_menu').css({
+            'right': '0',
+            'opacity': '0',
+            'visibility': 'hidden'
+        });
+    });
+    $('.ovelay_menu').click(function () {
+        CONTROL.main_sidenav.css({
+            'width': '0',
+        });
+        $('.ovelay_menu').css({
+            'right': '0',
+            'opacity': '0',
+            'visibility': 'hidden'
+        });
+    });
+});
+$(window).resize(function () {
+    CONTROL = {
+        main_LEFT: $('#container_main').offset().left,
+        btn_sidenav: $('.btn_sidenav'),
+        ovelay_menu: $('.ovelay_menu'),
+        main_sidenav: $('.main_sidenav '),
     }
-    $(document).ready(function () {
-        CONTROL={
-            main_LEFT: $('#container_main').offset().left,
-            main_sidenav : $('.main_sidenav '),
-        }
-    
+    CONTROL.main_sidenav.css({
+        'right': CONTROL.main_LEFT,
+    });
+    $('.btn_sidenav').click(function () {
         CONTROL.main_sidenav.css({
-            'right': CONTROL.main_LEFT,
+            'width': '270px',
         });
-        $('.btn_sidenav').click(function(){
-            CONTROL.main_sidenav.css({
-                'width': '270px',
-            });
-            $('.ovelay_menu').css({
-                'right': '270px',
-                'opacity': '1',
-                'visibility': 'visible'
-            });
-        });
-        $('#close_menu').click(function(){
-            CONTROL.main_sidenav.css({
-                'width': '0',
-            });
-            $('.ovelay_menu').css({
-                'right': '0',
-                'opacity': '0',
-                'visibility': 'hidden'
-            });
-        });
-        $('.ovelay_menu').click(function(){
-            CONTROL.main_sidenav.css({
-                'width': '0',
-            });
-            $('.ovelay_menu').css({
-                'right': '0',
-                'opacity': '0',
-                'visibility': 'hidden'
-            });
+        $('.ovelay_menu').css({
+            'right': '270px',
+            'opacity': '1',
+            'visibility': 'visible'
         });
     });
-    $(window).resize(function() {
-        CONTROL={
-            main_LEFT: $('#container_main').offset().left,
-            btn_sidenav: $('.btn_sidenav'),
-            ovelay_menu: $('.ovelay_menu'),
-            main_sidenav : $('.main_sidenav '),
-        }
+    $('#close_menu').click(function () {
         CONTROL.main_sidenav.css({
-            'right': CONTROL.main_LEFT,
+            'width': '0',
         });
-        $('.btn_sidenav').click(function(){
-            CONTROL.main_sidenav.css({
-                'width': '270px',
-            });
-            $('.ovelay_menu').css({
-                'right': '270px',
-                'opacity': '1',
-                'visibility': 'visible'
-            });
-        });
-        $('#close_menu').click(function(){
-            CONTROL.main_sidenav.css({
-                'width': '0',
-            });
-            $('.ovelay_menu').css({
-                'right': '0',
-                'opacity': '0',
-                'visibility': 'hidden'
-            });
-        });
-        $('.ovelay_menu').click(function(){
-            CONTROL.main_sidenav.css({
-                'width': '0',
-            });
-            $('.ovelay_menu').css({
-                'right': '0',
-                'opacity': '0',
-                'visibility': 'hidden'
-            });
+        $('.ovelay_menu').css({
+            'right': '0',
+            'opacity': '0',
+            'visibility': 'hidden'
         });
     });
-    
+    $('.ovelay_menu').click(function () {
+        CONTROL.main_sidenav.css({
+            'width': '0',
+        });
+        $('.ovelay_menu').css({
+            'right': '0',
+            'opacity': '0',
+            'visibility': 'hidden'
+        });
+    });
+});
+function slide_Show() {
+    var owl = $('#show_local');
+    owl.owlCarousel({
+        loop: false,
+        nav: false,
+        dots: false,
+        margin: 10,
+        mouseDrag: true,
+        touchDrag: true,
+        responsive: {
+            720: {
+                items: 3
+            },
+            320: {
+                items: 1
+            }
+        }
+    });
+}
+function show_Fillter() {
+    // console.log();
+    var WID_SCREEN = $(window).width()
+    var get_fitter = $('#get_fitter');
+    var hide_fitter = $('#hide_fitter');
 
+    get_fitter.click(function () {
+        $('.popup-fillter').css({
+            'left': '0',
+        });
+    });
+    hide_fitter.click(function () {
+        console.log(WID_SCREEN);
+        $('.popup-fillter').css({
+            'left':  - WID_SCREEN,
+        });
+    });
 
+} 
